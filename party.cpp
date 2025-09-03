17b545f3b854a9ced3c84d65a61265761360a398
msulli1355 <45134364+msulli1355@users.noreply.github.com>
2025-09-02 19:54:00 -0500

    Adding party class from

---
+++ party.cpp
@@ -0,0 +1,19 @@
+#include "party.h"
+
+void Party::addChar(Character*)
+{
+   //Add a Character to this party
+   //
+   //int partySize = sizeof(currCharArray)
+   //Character* newCharArray = Character[partySize + 1]
+   //currCharArray = newCharArray
+}
+
+void Party::deleteChar(Character*)
+{
+   //Remove a Character from this party
+}
+
+Character* Party::getMember(int)
+{
+}

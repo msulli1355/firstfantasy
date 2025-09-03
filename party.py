7b05268f9700c2da47a70657a170fe6b7716da4e
msulli1355 <45134364+msulli1355@users.noreply.github.com>
2025-09-02 20:08:51 -0500

    Add files via upload

---
+++ party.py
@@ -0,0 +1,31 @@
+'''
+#include "party.h"
+
+void Party::addChar(Character*)
+{
+   //Add a Character to this party
+   //
+   //int partySize = sizeof(currCharArray)
+   //Character* newCharArray = Character[partySize + 1]
+   //currCharArray = newCharArray
+}
+
+void Party::deleteChar(Character*)
+{
+   //Remove a Character from this party
+}
+
+Character* Party::getMember(int)
+{
+}
+'''
+class Party():
+   def __init__(self):
+      pass
+   def add_char(self):
+      pass
+   def delete_char(self):
+      pass
+   def get_member(self):
+      pass
+   
